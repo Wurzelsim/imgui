0ba7205cb7b3dcc198853d70145e01ee3d03446a
Wurzelsim <simon@unterwurzacher.at>
2022-09-07 23:28:55 +0200

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,53 @@
+project "imgui"
+	kind "staticLib"
+	language "C++"
+
+	targetdir ("build/bin/" .. outdir .. "/%{prj.name}")
+	objdir ("build/obj/" .. outdir .. "/%{prj.name}")
+
+	files {
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imgui_rectpack.h",
+		"imgui_textedit.h",
+		"imgui_truetype.h"
+	}
+
+
+	filter { "system:windows" }
+		staticruntime "on"
+		systemversion "latest"
+		cppdialect "C++17"
+
+		files {
+			"src/win32_init.c",
+			"src/win32_module.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.h",
+			"src/win32_time.c",
+			"src/win32_thread.h",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+		}
+		
+	filter { "configurations:Debug" }
+		symbols "on"
+
+	filter { "configurations:Release" }
+		optimize "on"
+
+	filter { "configurations:Dist" }
+		optimize "on"
+		
+		
+	
